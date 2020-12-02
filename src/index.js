f1e6c8c30755b211dd509e89b0d077062637f162
pomkalk <pomkalk@gmail.com>
2020-12-02 16:48:20 +0700

    router tested

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('asd')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+
+const App = () => {
+    return (<>
+        <Switch>
+            <Route path="/" exact={true}>
+                root <Link to="/login">GO TO LOGIN</Link>
+            </Route>
+            <Route path="/login">
+                login
+            </Route>
+        </Switch>
+    </>)
+}
+
+ReactDOM.render(<BrowserRouter basename="/admin"><App /></BrowserRouter>, document.getElementById('app'))
